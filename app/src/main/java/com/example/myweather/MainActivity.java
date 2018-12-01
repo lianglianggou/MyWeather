b4e79d98d1fe013434c4a945af4e76dc79c128cf
lianglianggou <317767301qq.com>
2018-12-01 20:14:45 +0800

    完成收藏功能

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,29 +1,311 @@
 package com.example.myweather;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.media.MediaPlayer;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import okhttp3.Call;
+
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView wendu,tishi,fengxiang,fengli,high,type,low,date;
+    MyDatabaseHelper dbmemo;
+    private String tianqi[];
+    Button bn;
+    TextView textView;
+    TextView city1;
 
+    private Handler handler=new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            if(msg.what==10)
+            {
+                String content=(String) msg.obj;
+                try {
+                    tianqi=today(content);
+                    wendu.setText("当前温度： \t"+tianqi[0]);
+                    tishi.setText("温馨提示： \t"+tianqi[1]);
+                    fengxiang.setText("风向： \t"+tianqi[2]);
+                    fengli.setText("风力： \t"+tianqi[3].substring(10,12));
+                    high.setText("最"+tianqi[4]);
+                    type.setText("天气类型： \t"+tianqi[5]);
+                    low.setText("最"+tianqi[6]);
+                    date.setText("日期： \t"+tianqi[7]);
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    };
+    private Handler handler1=new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            if(msg.what==10)
+            {
+                String content=(String) msg.obj;
+                try {
+                    tianqi=today1(content);
+                    wendu.setText("当前温度： \t"+tianqi[0]);
+                    tishi.setText("温馨提示： \t"+tianqi[1]);
+                    fengxiang.setText("风向： \t"+tianqi[2]);
+                    fengli.setText("风力： \t"+tianqi[3].substring(10,12));
+                    high.setText("最"+tianqi[4]);
+                    type.setText("天气类型： \t"+tianqi[5]);
+                    low.setText("最"+tianqi[6]);
+                    date.setText("日期： \t"+tianqi[7]);
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    };
+
+
+
+    protected String[] today(String content) throws Exception {
+        // TODO Auto-generated method stub
+        JSONObject obj=new JSONObject(content);
+        JSONObject data = obj.getJSONObject("data");
+        String wendu = data.getString("wendu");
+        String tishi=data.getString("ganmao");
+        JSONArray forecast = data.getJSONArray("forecast");
+        JSONObject obj2=(JSONObject) forecast.get(0);
+        String fengxiang=obj2.getString("fengxiang");
+        String fengli=obj2.getString("fengli");
+        String high=obj2.getString("high");
+        String type=obj2.getString("type");
+        String low=obj2.getString("low");
+        String date=obj2.getString("date");
+        String tianqi[]={wendu,tishi,fengxiang,fengli,high,type,low,date};
+        return tianqi;
+    }
+    protected String[] today1(String content) throws Exception {
+        // TODO Auto-generated method stub
+        JSONObject obj=new JSONObject(content);
+        JSONObject data = obj.getJSONObject("data");
+        String wendu = data.getString("wendu");
+        String tishi=data.getString("ganmao");
+        JSONArray forecast = data.getJSONArray("forecast");
+        JSONObject obj2=(JSONObject) forecast.get(1);
+        String fengxiang=obj2.getString("fengxiang");
+        String fengli=obj2.getString("fengli");
+        String high=obj2.getString("high");
+        String type=obj2.getString("type");
+        String low=obj2.getString("low");
+        String date=obj2.getString("date");
+        String tianqi[]={wendu,tishi,fengxiang,fengli,high,type,low,date};
+        return tianqi;
+    }
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        city1=(TextView)findViewById(R.id.chengshi) ;
+        Intent intent = getIntent();
+        final String city2 = intent.getStringExtra("city");
+        city1.setText(city2);
+        dbmemo=new MyDatabaseHelper(this,"love.db",null,1);
+        initViews();
+        bn=(Button)findViewById(R.id.first);
+        bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                try {
+                    myclick(view);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        bn=(Button)findViewById(R.id.second);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    myclick2(view);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        bn=(Button)findViewById(R.id.cllege);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a=city2;
+                String[] where={city2};
+
+                SQLiteDatabase db = dbmemo.getWritableDatabase();
+                // Cursor cursor = db.query("WordTable",null,null,null,null,null,null);
+                Cursor cursor =  db.query("love",new String[]{"city"},"city=?",where,null,null,null);
+
+                if(cursor.moveToFirst()){
+                    Toast.makeText(MainActivity.this,"已收藏",Toast.LENGTH_SHORT).show();
+                    cursor.close();
+
+                }else{
+
+                    ContentValues values=new ContentValues();
+                    values.put("city",a);
+                    db.insert("love",null,values);
+                    Toast.makeText(MainActivity.this,"收藏成功",Toast.LENGTH_SHORT).show();}
+
+            }
+        });
+        bn=(Button)findViewById(R.id.back);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Weather.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    private String getCity() {
+        // TODO Auto-generated method stub
+        String city=city1.getText().toString().trim();
+        return city;
+    }
+    private void initViews() {
+        // TODO Auto-generated method stub
+
+        wendu =(TextView) findViewById(R.id.wendu);
+        tishi =(TextView) findViewById(R.id.tishi);
+        fengxiang =(TextView) findViewById(R.id.fengxiang);
+        fengli =(TextView) findViewById(R.id.fengli);
+        high =(TextView) findViewById(R.id.high);
+        type =(TextView) findViewById(R.id.type);
+        low=(TextView) findViewById(R.id.low);
+        date =(TextView) findViewById(R.id.date);
+
+    }
+
+    public void myclick(View v) throws Exception
+    {;
+        Request request;
+        OkHttpClient client= new OkHttpClient();
+        Request.Builder builder=new Request.Builder();
+        builder.get();
+        builder.url("http://wthrcdn.etouch.cn/weather_mini?city="+ URLEncoder.encode(getCity(), "UTF-8"));
+        request = builder.build();
+        Call call=client.newCall(request);
+
+        call.enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String content = response.body().string();
+                Message msg=Message.obtain();
+                msg.obj=content;
+                msg.what=10;
+                handler.sendMessage(msg);
             }
         });
     }
+    public void myclick2(View v) throws Exception
+    {;
+        Request request;
+        OkHttpClient client= new OkHttpClient();
+        Request.Builder builder=new Request.Builder();
+        builder.get();
+        builder.url("http://wthrcdn.etouch.cn/weather_mini?city="+ URLEncoder.encode(getCity(), "UTF-8"));
+        request = builder.build();
+        Call call=client.newCall(request);
 
+        call.enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String content = response.body().string();
+                Message msg=Message.obtain();
+                msg.obj=content;
+                msg.what=10;
+                handler1.sendMessage(msg);
+            }
+        });
+    }
+    class MyDatabaseHelper extends SQLiteOpenHelper {
+        public static final String CREATE_LOVE = "create table love ("
+                + "city text" + ")";
+
+        private Context mContext;
+
+        public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+            super(context, name, factory, version);
+
+            mContext = context;
+
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_LOVE);
+            Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
 }
--- app/src/main/java/com/example/myweather/Weather.java
+++ app/src/main/java/com/example/myweather/Weather.java
@@ -3,6 +3,13 @@ package com.example.myweather;
 import android.app.DownloadManager;
 import android.app.Notification;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.media.MediaPlayer;
 
 import android.os.Handler;
@@ -12,11 +19,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -24,6 +37,7 @@ import org.json.JSONObject;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 
@@ -38,58 +52,19 @@ import okhttp3.Response;
 public class Weather extends AppCompatActivity {
     private EditText city1;
     private TextView wendu,tishi,fengxiang,fengli,high,type,low,date;
-
+    MyDatabaseHelper dbmemo;
     private String tianqi[];
     Button bn;
+    EditText textView;
+    ListView mListView;
+    ArrayList<String> date1=new ArrayList<>();
+    int count=0;
 
-    private Handler handler=new Handler(){
-        public void handleMessage(android.os.Message msg) {
-            if(msg.what==10)
-            {
-                String content=(String) msg.obj;
-                try {
-                    tianqi=today(content);
-                    wendu.setText("当前温度： \t"+tianqi[0]);
-                    tishi.setText("温馨提示： \t"+tianqi[1]);
-                    fengxiang.setText("风向： \t"+tianqi[2]);
-                    fengli.setText("风力： \t"+tianqi[3].substring(10,12));
-                    high.setText("最"+tianqi[4]);
-                    type.setText("天气类型： \t"+tianqi[5]);
-                    low.setText("最"+tianqi[6]);
-                    date.setText("日期： \t"+tianqi[7]);
 
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
 
-        }
-    };
-    private Handler handler1=new Handler(){
-        public void handleMessage(android.os.Message msg) {
-            if(msg.what==10)
-            {
-                String content=(String) msg.obj;
-                try {
-                    tianqi=today1(content);
-                    wendu.setText("当前温度： \t"+tianqi[0]);
-                    tishi.setText("温馨提示： \t"+tianqi[1]);
-                    fengxiang.setText("风向： \t"+tianqi[2]);
-                    fengli.setText("风力： \t"+tianqi[3].substring(10,12));
-                    high.setText("最"+tianqi[4]);
-                    type.setText("天气类型： \t"+tianqi[5]);
-                    low.setText("最"+tianqi[6]);
-                    date.setText("日期： \t"+tianqi[7]);
 
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
 
-        }
-    };
+
     private String getCity() {
         // TODO Auto-generated method stub
         String city=city1.getText().toString().trim();
@@ -114,23 +89,7 @@ public class Weather extends AppCompatActivity {
         String tianqi[]={wendu,tishi,fengxiang,fengli,high,type,low,date};
         return tianqi;
     }
-    protected String[] today1(String content) throws Exception {
-        // TODO Auto-generated method stub
-        JSONObject obj=new JSONObject(content);
-        JSONObject data = obj.getJSONObject("data");
-        String wendu = data.getString("wendu");
-        String tishi=data.getString("ganmao");
-        JSONArray forecast = data.getJSONArray("forecast");
-        JSONObject obj2=(JSONObject) forecast.get(1);
-        String fengxiang=obj2.getString("fengxiang");
-        String fengli=obj2.getString("fengli");
-        String high=obj2.getString("high");
-        String type=obj2.getString("type");
-        String low=obj2.getString("low");
-        String date=obj2.getString("date");
-        String tianqi[]={wendu,tishi,fengxiang,fengli,high,type,low,date};
-        return tianqi;
-    }
+
     @Override
 
 
@@ -138,105 +97,184 @@ public class Weather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        initViews();
-        bn=(Button)findViewById(R.id.first);
+        dbmemo=new MyDatabaseHelper(this,"love.db",null,1);
+        mListView=(ListView) findViewById(R.id.listview);
+        SQLiteDatabase db=dbmemo.getWritableDatabase();
+        Cursor cursor=db.query("love",null,null,null,null,null,null);
+
+        if(cursor.moveToFirst()){
+            do{
+                String author=cursor.getString(cursor.getColumnIndex("city"));
+                date1.add(count++,author);
+            }while (cursor.moveToNext());
+            cursor.close();
+            //oast.makeText(MainActivity.this,"success",Toast.LENGTH_SHORT).show();
+        }
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(Weather.this,R.layout.support_simple_spinner_dropdown_item,date1);
+        ListView listView=(ListView)findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s=date1.get(position);
+                Intent intent=new Intent(Weather.this,MainActivity.class);
+                intent.putExtra("city",s);
+                startActivity(intent);
+            }
+        });
+        ItemOnLongClick1();
+
+
+        textView=(EditText) findViewById(R.id.city) ;
+        dbmemo=new MyDatabaseHelper(this,"love.db",null,1);
+        dbmemo.getWritableDatabase();
+        bn=(Button)findViewById(R.id.see);
         bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                try {
-                    myclick(view);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
+                String a=textView.getText().toString();
+                Intent intent=new Intent(Weather.this,MainActivity.class);
+                intent.putExtra("city","河南");
+                startActivity(intent);
 
             }
         });
-        bn=(Button)findViewById(R.id.second);
+        bn=(Button)findViewById(R.id.college);
         bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //String a=textView.getText().toString();
+                String a="河北";
+                String[] where={"河北"};
 
-                try {
-                    myclick2(view);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                SQLiteDatabase db = dbmemo.getWritableDatabase();
+                // Cursor cursor = db.query("WordTable",null,null,null,null,null,null);
+                Cursor cursor =  db.query("love",new String[]{"city"},"city=?",where,null,null,null);
 
+                if(cursor.moveToFirst()){
+                    Toast.makeText(Weather.this,"已收藏",Toast.LENGTH_SHORT).show();
+                    cursor.close();
+
+                }else{
+
+                    ContentValues values=new ContentValues();
+                    values.put("city",a);
+                    db.insert("love",null,values);
+                    date1.add(count++,a);
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(Weather.this,R.layout.support_simple_spinner_dropdown_item,date1);
+                    ListView listView=(ListView)findViewById(R.id.listview);
+                    listView.setAdapter(adapter);
+                    Toast.makeText(Weather.this,"收藏成功",Toast.LENGTH_SHORT).show();}
+            }
+        });
+
+    }
+
+    private void ItemOnLongClick1() {
+        mListView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+
+            public void onCreateContextMenu(ContextMenu menu, View v,
+                                            ContextMenu.ContextMenuInfo menuInfo) {
+                menu.add(0, 0, 0, "删除");
+                menu.add(0, 1, 0, "上升");
+                menu.add(0, 2, 0, "置顶");
 
             }
         });
     }
-    private void initViews() {
-        // TODO Auto-generated method stub
-        city1=(EditText) findViewById(R.id.city);
-        wendu =(TextView) findViewById(R.id.wendu);
-        tishi =(TextView) findViewById(R.id.tishi);
-        fengxiang =(TextView) findViewById(R.id.fengxiang);
-        fengli =(TextView) findViewById(R.id.fengli);
-        high =(TextView) findViewById(R.id.high);
-        type =(TextView) findViewById(R.id.type);
-        low=(TextView) findViewById(R.id.low);
-        date =(TextView) findViewById(R.id.date);
+    public boolean onContextItemSelected(MenuItem item) {
+        dbmemo=new MyDatabaseHelper(this,"love.db",null,1);
+        SQLiteDatabase db=dbmemo.getWritableDatabase();
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+                .getMenuInfo();
+        int MID = (int) info.id;// 这里的info.id对应的就是数据库中_id的值
+
+        switch (item.getItemId()) {
+            case 0:
+                String[] where={date1.get(MID)};
+                db.delete("love","city=?",where);
+                date1.remove(MID);
+                ArrayAdapter<String> adapter=new ArrayAdapter<String>(Weather.this,R.layout.support_simple_spinner_dropdown_item,date1);
+                ListView listView=(ListView)findViewById(R.id.listview);
+                listView.setAdapter(adapter);
+                break;
+
+            case 1:
+                String xx=date1.get(MID);
+                String yy=date1.get(MID-1);
+                final String[] where2={date1.get(MID-1)};
+                final String[] where3={date1.get(MID)};
+                final String[] where4={"*"};
+                ContentValues values0=new ContentValues();
+                values0.put("city","*");
+                ContentValues values=new ContentValues();
+                values.put("city",xx);
+                db.update("love",values0,"city=?",where2);
+                date1.set(MID-1,xx);
+                ContentValues values1=new ContentValues();
+                values1.put("city",yy);
+                db.update("love",values1,"city=?",where3);
+                db.update("love",values,"city=?",where4);
+                date1.set(MID,yy);
+                ArrayAdapter<String> adapter1=new ArrayAdapter<String>(Weather.this,R.layout.support_simple_spinner_dropdown_item,date1);
+                ListView listView1=(ListView)findViewById(R.id.listview);
+                listView1.setAdapter(adapter1);
+                break;
+            case 2:
+                String xxx=date1.get(MID);
+                String yyy=date1.get(0);
+                final String[] where22={date1.get(0)};
+                final String[] where33={date1.get(MID)};
+                final String[] where44={"*"};
+                ContentValues values00=new ContentValues();
+                values00.put("city","*");
+                ContentValues values2=new ContentValues();
+                values2.put("city",xxx);
+                db.update("love",values00,"city=?",where22);
+                date1.set(0,xxx);
+                ContentValues values3=new ContentValues();
+                values3.put("city",yyy);
+                db.update("love",values3,"city=?",where33);
+                db.update("love",values2,"city=?",where44);
+                date1.set(MID,yyy);
+                ArrayAdapter<String> adapter2=new ArrayAdapter<String>(Weather.this,R.layout.support_simple_spinner_dropdown_item,date1);
+                ListView listView2=(ListView)findViewById(R.id.listview);
+                listView2.setAdapter(adapter2);
+                break;
+            default:
+                break;
+        }
+
+        return super.onContextItemSelected(item);
 
     }
 
-    public void myclick(View v) throws Exception
-    {;
-        Request request;
-        OkHttpClient client= new OkHttpClient();
-        Request.Builder builder=new Request.Builder();
-        builder.get();
-        builder.url("http://wthrcdn.etouch.cn/weather_mini?city="+ URLEncoder.encode("天津", "UTF-8"));
-        request = builder.build();
-        Call call=client.newCall(request);
+    class MyDatabaseHelper extends SQLiteOpenHelper {
+        public static final String CREATE_LOVE = "create table love ("
+                + "city text" + ")";
 
-        call.enqueue(new Callback() {
+        private Context mContext;
 
+        public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
 
-            @Override
-            public void onFailure(Call call, IOException e) {
+            super(context, name, factory, version);
 
-            }
+            mContext = context;
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String content = response.body().string();
-                Message msg=Message.obtain();
-                msg.obj=content;
-                msg.what=10;
-                handler.sendMessage(msg);
-            }
-        });
-    }
-    public void myclick2(View v) throws Exception
-    {;
-        Request request;
-        OkHttpClient client= new OkHttpClient();
-        Request.Builder builder=new Request.Builder();
-        builder.get();
-        builder.url("http://wthrcdn.etouch.cn/weather_mini?city="+ URLEncoder.encode("天津", "UTF-8"));
-        request = builder.build();
-        Call call=client.newCall(request);
+        }
 
-        call.enqueue(new Callback() {
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_LOVE);
+            Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+        }
 
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String content = response.body().string();
-                Message msg=Message.obtain();
-                msg.obj=content;
-                msg.what=10;
-                handler1.sendMessage(msg);
-            }
-        });
+        }
     }
 
 }
